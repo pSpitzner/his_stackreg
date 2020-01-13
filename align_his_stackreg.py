2a220d86e68051a9b7f51083a8fd8e1454634449
Paul@Alublech <sitzkringel@web.de>
2020-01-13 16:30:02 +0100

    adjusting exposure helped a lot

--- align_his_stackreg.py
+++ align_his_stackreg.py
@@ -2,40 +2,55 @@
 # Uses his_opener to load the first frame of each .HIS stack.
 # Then use stackreg to align them to the chosen reference image
 # and obtains the transformations to apply on provided extracted ROIs
-#
 # ------------------------------------------------------------------ #
 
 import numpy as np
 
 # provide one image or his file as reference to which the others are aligned
-ref_img_file = "/Volumes/exfat/experiments/paul/stacks/191218_3_div12_prestim.HIS"
+ref_img_file = "I:/PAUL/191218_3_div10_prestim.HIS"
 
 # provide the set of ROIs that were found in the ref_img_file
-ref_roi_file = "/Volumes/exfat/experiments/paul/stacks/191218_3_div10_instim_off_ROI_centers_everything_image.csv"
+ref_roi_file = "D:/experiments/paul/register/dat/191218_3_div10_prestim_ROI_centers_everything.orig.roi.csv"
 
 # the list of files for which aligned ROIs are wanted
 mov_img_file_list = [
-    "/Volumes/exfat/experiments/paul/stacks/191218_3_div12_instim_on.HIS",
-    "/Volumes/exfat/experiments/paul/stacks/191218_3_div12_poststim.HIS",
-    "/Volumes/exfat/experiments/paul/stacks/191218_3_div13_instim_on.HIS",
+    "I:/PAUL/191218_3_div10_instim_off.HIS",
+    "I:/PAUL/191218_3_div10_instim_on.HIS",
+    "I:/PAUL/191218_3_div12_instim_on.HIS",
+    "I:/PAUL/191218_3_div12_poststim.HIS",
+    "I:/PAUL/191218_3_div12_prestim.HIS",
+    "I:/PAUL/191218_3_div13_instim_on.HIS",
+    "I:/PAUL/191218_3_div13_poststim.HIS",
+    "I:/PAUL/191218_3_div13_pretstim.HIS",
+    "I:/PAUL/191218_3_div15_instim_on.HIS",
+    "I:/PAUL/191218_3_div15_poststim.HIS",
+    "I:/PAUL/191218_3_div15_pretstim.HIS",
+    "I:/PAUL/191218_3_div16_instim_on.HIS",
+    "I:/PAUL/191218_3_div16_poststim.HIS",
+    "I:/PAUL/191218_3_div16_pretstim.HIS",
+    "I:/PAUL/191218_3_div8_instim_on.HIS",
+    "I:/PAUL/191218_3_div8_instim_off.HIS",
+    "I:/PAUL/191218_3_div8_prestim.HIS",
 ]
 
 # where to save the aligned ROIs.
-mov_roi_save_to = "/Volumes/exfat/experiments/paul/stacks/roi/"
+mov_roi_saveto = "D:/experiments/paul/register/dat/roi/"
+# size of the square to assign to each roi
+roi_width = 10
 
-# where to save the image previews with aligned rois drawn
+# where to save the image previews with aligned rois drawn.
 # this folder is checked for existing files and if the target is found,
 # it will not be overwritten. Therefore, you can just delete the incorrectly matched
 # images and run again with different parameters
-mov_img_save_to = "/Volumes/exfat/experiments/paul/stacks/img/"
+mov_img_saveto = "D:/experiments/paul/register/dat/img/"
 
 
 # use only the first frame of each stack or compute an average. average takes longer
-# as the whole stack might have to be checked. In my tests, the check took around
-# 10sec for a 45GB from SSD, from HD avoid this
+# as the whole stack might have to be checked but produces much better results.
+# how many frames to draw for the whole stack (spread evenly from beginning to end)
 use_average = True
-# frames_for_average = 'auto'
-frames_for_average = np.arange(0, 100)
+frames_for_average = 1000
+
 
 # load the regions of interest. we are using image coordinates!
 ref_roi_dat = np.loadtxt(ref_roi_file, delimiter=",", skiprows=1)
@@ -51,9 +66,11 @@ roid = ref_roi_dat[:, 0]  # id of the region of interest
 
 from skimage import io
 from skimage import transform as tf
+from skimage import exposure
 from pystackreg import StackReg  # pip install pystackreg
 
 import utility as ut
+import os
 import os.path as op
 from his_opener import HisOpener
 
@@ -66,16 +83,20 @@ except ImportError:
     plt = None
 
 # check which files to produce
-mov_roi_save_to_list = []
-mov_img_save_to_list = []
+mov_roi_saveto_list = []
+mov_img_saveto_list = []
 mov_img_already_done = []
 for idx, file in enumerate(mov_img_file_list):
     base = ut.base_name(file)
-    roi_path = op.join(op.abspath(mov_roi_save_to), base) + "_roi.csv"
-    img_path = op.join(op.abspath(mov_img_save_to), base) + "_roi.png"
-    mov_roi_save_to_list.append(roi_path)
-    mov_img_save_to_list.append(img_path)
+    roi_path = op.join(op.abspath(mov_roi_saveto), base) + "_roi.csv"
+    img_path = op.join(op.abspath(mov_img_saveto), base) + "_roi.png"
+    mov_roi_saveto_list.append(roi_path)
+    mov_img_saveto_list.append(img_path)
     mov_img_already_done.append(op.exists(img_path))
+
+# create output folders
+os.makedirs(mov_img_saveto, mode=0o777, exist_ok=True)
+os.makedirs(mov_roi_saveto, mode=0o777, exist_ok=True)
 
 # create list of points from the original ROIs
 ref_points = np.vstack((cols, rows, roid)).T
@@ -89,11 +110,22 @@ else:
 width = ref_his.width
 height = ref_his.height
 
-# meh quick and dirty
+# increase the image contrast. this improved the results from stackreg drastically
+# https://scikit-image.org/docs/dev/auto_examples/color_exposure/plot_equalize.html#sphx-glr-auto-examples-color-exposure-plot-equalize-py
+p2, p98 = np.percentile(ref_img, (2, 98))
+ref_img = exposure.rescale_intensity(ref_img, in_range=(p2, p98))
+# ref_img = exposure.equalize_hist(ref_img)
+
+# save the original rois just for good measure
+x, y, i = ref_points[:].T
+temp = op.join(op.abspath(mov_roi_saveto), ut.base_name(ref_img_file))
+ut.save_rois(fname=f"{temp}_roi.csv", roi_id=i, x=x, y=y, roi_width=roi_width)
+
+# quick and dirty, export the original for comparison
 if plt is not None:
     ref_mask = np.zeros(shape=(width, height, 4), dtype="uint8")
     for point in ref_points:
-        ut.paint_roi(point[0], point[1], 20, ref_mask, channel=0)
+        ut.paint_roi(point[0], point[1], roi_width, ref_mask, channel=0)
 
     # create a figure, this involves some ugly code to get rid of the borders
     bbox = matplotlib.transforms.Bbox(((0, 0), (width / 100, height / 100)))
@@ -104,29 +136,36 @@ if plt is not None:
     # here we plot both things
     ax.imshow(ref_img, cmap="gray")
     ax.imshow(ref_mask)
-    temp = op.join(op.abspath(mov_img_save_to), ut.base_name(ref_img_file))
-    plt.savefig(f"{temp}.png", bbox_inches=bbox, pad_inches=0.0, dpi=100)
+    temp = op.join(op.abspath(mov_img_saveto), ut.base_name(ref_img_file))
+    plt.savefig(f"{temp}_roi.png", bbox_inches=bbox, pad_inches=0.0, dpi=100)
 
-# save the original rois just for good measure
 
 # process every target stack
-for idx, file_path in enumerate(mov_img_file_list):
+for idx, mov_img_path in enumerate(mov_img_file_list):
     if mov_img_already_done[idx]:
-        print(f"Skipping {file_path}")
+        print(f"Skipping {mov_img_path}")
         continue
-    mov_his = HisOpener(file_path, skip_consistency_check=True)
+    mov_his = HisOpener(mov_img_path, skip_consistency_check=True)
     if use_average:
         mov_img = mov_his.read_frame_average(frames_for_average)
     else:
         mov_img = mov_his.read_frame(0)
     del mov_his
 
-    print(f"Aligning {file_path}")
+    print(f"Aligning {mov_img_path}")
+
+    # increase the image contrast. this improved the results from stackreg drastically
+    p2, p98 = np.percentile(mov_img, (2, 98))
+    mov_img = exposure.rescale_intensity(mov_img, in_range=(p2, p98))
 
     # find the transformation matrix
     # we only use rigid body, gives: x shift, y shift, rotation
     sreg = StackReg(StackReg.RIGID_BODY)
     tmat = sreg.register(ref=ref_img, mov=mov_img)
+
+    # if average_over_experiments:
+    #     out_img = sreg.transform(mov=mov_img, tmat=tmat)
+    #     ref_img = np.mean([ref_img, out_img], axis=0)
 
     # apply the matrix to the old ROIs
     # this is essentially just:
@@ -135,18 +174,17 @@ for idx, file_path in enumerate(mov_img_file_list):
     mov_points = np.copy(ref_points)
     mov_points[:, 0:2] = tf.matrix_transform(coords=ref_points[:, 0:2], matrix=tmat)
 
-    # save in the format that works with netcal (legacy): id | x | y | roi_width
+    # save in netcals image format. import via "load roi (legacy)"
     x, y, i = mov_points[:].T
-    out_dat = np.vstack((i, x, y, np.ones_like(x) * 10)).T
-    np.savetxt(
-        f"{out_path}.roi.csv", out_dat, fmt="%d,%.2f,%.2f,%d", header="ROI,X,Y,Width"
+    ut.save_rois(
+        fname=mov_roi_saveto_list[idx], roi_id=i, x=x, y=y, roi_width=roi_width
     )
 
     # plot to check if alignment worked and compare
     if plt is not None:
         mov_mask = np.zeros(shape=(width, height, 4), dtype="uint8")
         for point in mov_points:
-            ut.paint_roi(point[0], point[1], 20, mov_mask, channel=2)
+            ut.paint_roi(point[0], point[1], roi_width, mov_mask, channel=2)
 
         # create a figure, this involves some ugly code to get rid of the borders
         bbox = matplotlib.transforms.Bbox(((0, 0), (width / 100, height / 100)))
@@ -157,7 +195,9 @@ for idx, file_path in enumerate(mov_img_file_list):
         # but here we plot both things
         ax.imshow(mov_img, cmap="gray")
         ax.imshow(mov_mask)
-        plt.savefig(f"{out_path}.png", bbox_inches=bbox, pad_inches=0.0, dpi=100)
+        plt.savefig(
+            f"{mov_img_saveto_list[idx]}", bbox_inches=bbox, pad_inches=0.0, dpi=100
+        )
 
 
 # to transform an image
--- utility.py
+++ utility.py
@@ -8,7 +8,7 @@ def paint_roi(col, row, width, img, channel=0, alpha=123):
     """
     row = int(row)
     col = int(col)
-    d = int(np.ceil(width / 2))
+    d = int(np.floor(width / 2))
     grid = np.mgrid[col - d : col + d, row - d : row + d]
     grid[grid < 0] = 0
     grid[0][grid[0] >= img.shape[0]] = img.shape[0] - 1
@@ -29,7 +29,7 @@ def base_name(source_file, extension_delim="."):
     """
         only keep the last part of the folder hierarchy and remove the last extension
     """
-    file_name = os.path.base_name(source_file)
+    file_name = os.path.basename(source_file)
     # file_name = source_file.split(folder_delim)[-1]
     base_name = file_name.split(extension_delim)[:-1]
     if len(base_name) == 0:
@@ -37,4 +37,19 @@ def base_name(source_file, extension_delim="."):
     base_name = '.'.join(map(str, base_name))
     return base_name
 
-def save_rois()
+def save_rois(fname, roi_id, x, y, roi_width, img_width=1024):
+    """
+        save in the format that works with netcal (legacy): id | x | y | roi_width
+        netcal does not like floats fort the coordinates and
+        comments have to be indicated with %
+    """
+    flagged = np.zeros_like(x, dtype=bool)
+    flagged[np.where(x > img_width-roi_width/2)] = True
+    flagged[np.where(y > img_width-roi_width/2)] = True
+    flagged[np.where(x < roi_width/2)] = True
+    flagged[np.where(y < roi_width/2)] = True
+    for idx, roi in enumerate(roi_id[flagged]):
+        print(f"ROI {roi} at {x[idx]} | {y[idx]} might be out of the image")
+    out_dat = np.vstack((roi_id, x, y, np.ones_like(x) * roi_width)).T
+    np.savetxt(fname, out_dat, fmt="%d,%.0f,%.0f,%d", header="ROI,X,Y,Width", comments="%"
+    )
